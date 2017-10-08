4bb7f90d6bc20e95ff037d5c8f8367e7dac4210b
giulira <giulira@gmail.com>
2017-10-08 02:18:10 -0300

    Update LivroController.java

--- src/main/java/br/com/fiap/controller/LivroController.java
+++ src/main/java/br/com/fiap/controller/LivroController.java
@@ -27,7 +27,7 @@ public class LivroController {
 	
 	@GetMapping("/livroById/{id}")
 	public ResponseEntity<?> find(@PathVariable("id") int id) {
-		System.out.println("Aeeeeeeeeeeeeeeeeeeeee123456");
+		System.out.println("Aeeeeeeeeeeeeeeeeeeeee1234567890");
 		Livro livro = livroDAO.getLivroById(id);
 		if (livro == null) {
 			return new ResponseEntity<>("Livro não existe com id: " + id, HttpStatus.NOT_FOUND);
